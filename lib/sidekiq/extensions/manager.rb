aeb7c3dcf3ff60a61fa6e666ae9052e9b5d35ad8
sm <preeti.gupta@goshopmatic.com>
2022-08-05 13:04:38 +0530

    Fixed undefined method options error in manager.rb

--- lib/sidekiq/extensions/manager.rb
+++ lib/sidekiq/extensions/manager.rb
@@ -2,11 +2,12 @@ class Sidekiq::Manager
   module InitLimitFetch
     def initialize(options={})
       options[:fetch] = Sidekiq::LimitFetch
+      @options = options
       super
     end
 
     def start
-      Sidekiq::LimitFetch::Queues.start options
+      Sidekiq::LimitFetch::Queues.start @options
       Sidekiq::LimitFetch::Global::Monitor.start!
       super
     end
